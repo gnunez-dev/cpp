25aba747a501d00f3c2b2f782e25be48bb88d2bc
gnunez-dev <gnunez.dev@gmail.com>
2023-06-24 12:03:39 -0500

    variables and const

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -6,5 +6,26 @@ int main() {
   // *std -> is for the standard library
   // *cout -> character output
   std::cout << "Hello World";
+
+  /* 
+    //Variables
+    // int
+    int a = 1;
+    int b = 2;
+    int temp = a;
+    a = b;
+    b = temp;
+
+    // double
+    double c = 2.5
+    std::cout << b;
+  */
+
+  /*
+    //constants
+    const double pi = 3.14
+  */
+
+
   return 0;
 }

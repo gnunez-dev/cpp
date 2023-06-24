163d4949a154f6f64f6a60d458d863e987e3be6d
gnunez-dev <gnunez.dev@gmail.com>
2023-06-24 08:47:38 -0500

    fixed typos in comments

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream> //this is part of the standard librery, this is for printing in the screen
+#include <iostream> //this is part of the standard library, this is for printing on the screen
 
 //the main fn is the entry point to our program.
-//This fn has to return a int number, if all is ok return 0, otherwise another number is returned and is understood as an error
+//This fn has to return an int number, if all is ok it returns 0, otherwise, another number is returned and is understood as an error
 int main() {
-  // *std -> is for the standard librery
+  // *std -> is for the standard library
   // *cout -> character output
   std::cout << "Hello World";
   return 0;

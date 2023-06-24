7db24f080696259fef0b8857c65356628cd50517
gnunez-dev <gnunez.dev@gmail.com>
2023-06-24 07:45:45 -0500

    first commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //this is part of the standard librery, this is for printing in the screen
+
+//the main fn is the entry point to our program.
+//This fn has to return a int number, if all is ok return 0, otherwise another number is returned and is understood as an error
+int main() {
+  // *std -> is for the standard librery
+  // *cout -> character output
+  std::cout << "Hello World";
+  return 0;
+}

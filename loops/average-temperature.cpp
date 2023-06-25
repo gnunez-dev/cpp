5e96d34c46d43c985582cfc700a90fd7c932fd5e
gnunez-dev <gnunez.dev@gmail.com>
2023-06-25 07:59:52 -0500

    added guess-number and added a space in averange-temperature

--- loops/average-temperature.cpp
+++ loops/average-temperature.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int  main(){
   int temperatures[] = { 60, 80, 90 };
   double sum = 0;
+
   for(int temperature: temperatures)
     sum += temperature;
+    
   short count = sizeof(temperatures) / sizeof(int);
   double averange = sum / count;
 
---
+++ loops/guess-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  short secretNum = 9;
+
+  short num;
+  
+  while(secretNum != num){
+    cout << "Guess a number: ";
+    cin >> num;
+  }
+
+  return 0;
+}

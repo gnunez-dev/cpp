c158406deffe29998f03315d61baa7d72e66a2ae
gnunez-dev <gnunez.dev@gmail.com>
2023-06-25 01:18:04 -0500

    added average-temperature

---
+++ loops/average-temperature.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int  main(){
+  int temperatures[] = { 60, 80, 90 };
+  double sum = 0;
+  for(int temperature: temperatures)
+    sum += temperature;
+  short count = sizeof(temperatures) / sizeof(int);
+  double averange = sum / count;
+
+  cout << "The averange is: " << averange << endl;
+  return 0;
+}

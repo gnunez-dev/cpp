252bd37d596c2b1b1c3d7f5866ac0c9ed4829733
gnunez-dev <gnunez.dev@gmail.com>
2023-06-25 08:24:03 -0500

    added rows

---
+++ loops/rows.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int rows;
+  cout << "Enter a row number: ";
+  cin >> rows;
+
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < i; j++)
+      cout << "*";
+    cout << "*" << endl;
+  }
+
+  return 0;
+}

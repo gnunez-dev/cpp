b2c045421b0a77b6eeabf0dbc3937c72cc970351
gnunez-dev <gnunez.dev@gmail.com>
2023-07-09 21:06:01 -0500

    added theRelationshipBetweenArraysAndPointers~

--- pointers/theRelationshipBetweenArraysAndPointers.cpp
+++ pointers/theRelationshipBetweenArraysAndPointers.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 using namespace std;
 
+//the arrays are pass as references...
+void printArray(int numbers[]){
+  numbers[0] = 56;
+}
+
 int main() {
-  
-  cout << "";
+  int numbers[] = {2, 5, 9};
+  printArray(numbers);
+  cout << numbers[0];
 
   return 0;
 }

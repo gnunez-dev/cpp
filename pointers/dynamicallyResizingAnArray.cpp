a5c00997260bbef666a2ee853af477b71ab18ff8
gnunez-dev <gnunez.dev@gmail.com>
2023-07-09 21:50:09 -0500

    added dynamicallyResizingAnArray

--- pointers/dynamicallyResizingAnArray.cpp
+++ pointers/dynamicallyResizingAnArray.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 using namespace std;
 
+//you can use vector for dynamic arrays
 int main() {
+  int capacity = 5;
+  int* numbers = new int[capacity];
+  int entries = 0;
+
+  while(true){
+    cout << "Number: ";
+    cin >> numbers[entries];
+    if(cin.fail()) break;
+    entries++;
+    
+    if (entries == capacity){
+      capacity *= 2;
+      int* temp = new int[capacity];
+      for(int i = 0; i < entries; i++){
+        temp[i] = numbers[i];
+      }
+      delete[] numbers;
+      numbers = temp;
+    }
+  }
+
+  for(int i = 0; i < entries; i++){
+    cout << numbers[i] << " ";
+  }
   
-  cout << "";
+  delete[] numbers;
 
   return 0;
 }

032813c69a861b36b85da608e55a4de749012345
gnunez-dev <gnunez.dev@gmail.com>
2023-07-09 21:15:22 -0500

    added pointersArithmetic

--- pointers/pointersArithmetic.cpp
+++ pointers/pointersArithmetic.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//you can online use addition and subtraction in pointers
+//ptr => numbers[0]
+//ptr++ => numbers[1]
+//ptr-- => numbers[0]
+
 int main() {
-  
-  cout << "";
+  int numbers[] = { 5, 9, 2};
+  int* ptr = numbers;
+
+  //same operation
+  cout << "*(ptr + 1): " << *(ptr + 1) << endl
+       << "ptr[1]: " << ptr[1] << endl
+       << "numbers[1]: " << numbers[1] << endl;
 
   return 0;
 }

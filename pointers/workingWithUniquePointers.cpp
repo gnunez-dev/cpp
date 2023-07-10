b41d9502401a3db93fe3f8bbf604ddca14acddb3
gnunez-dev <gnunez.dev@gmail.com>
2023-07-09 22:03:43 -0500

    added workingWithUniquePointers

--- pointers/workingWithUniquePointers.cpp
+++ pointers/workingWithUniquePointers.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <memory>
+
 using namespace std;
 
+//we cannot perform any arihmatic operations in unique pointers
 int main() {
-  
-  cout << "";
+  auto numbers = make_unique<int[]>(10);
+  auto y = make_unique<int>();
+
+  numbers[0] = 10;
+  cout << numbers[0];
 
   return 0;
 }

916625e2b32f1bc49c5a2f4bb5430c03b68422a4
gnunez-dev <gnunez.dev@gmail.com>
2023-07-09 22:10:07 -0500

    added workingWithSharedPointers

--- pointers/workingWithSharedPointers.cpp
+++ pointers/workingWithSharedPointers.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <memory>
+
 using namespace std;
 
 int main() {
   
-  cout << "";
+  //shared_ptr<int> x(new int);
+  auto x = make_shared<int>();
+  *x = 10;
+
+  shared_ptr<int> y(x);
+  if(x == y)
+    cout << "Equal" << endl;
+  
+  cout << "x: " << *x << " y:" << *y << endl;
 
   return 0;
 }
--- pointers/workingWithUniquePointers.cpp
+++ pointers/workingWithUniquePointers.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 //we cannot perform any arihmatic operations in unique pointers
+//with this way to create pointers, we don't need to delete the memory, the function cleanup for us
 int main() {
   auto numbers = make_unique<int[]>(10);
   auto y = make_unique<int>();

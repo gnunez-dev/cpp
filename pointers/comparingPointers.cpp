798b62f202b78601680ef395e9c8a3ee877e25a5
gnunez-dev <gnunez.dev@gmail.com>
2023-07-09 21:23:51 -0500

    added comparingPointers

--- pointers/comparingPointers.cpp
+++ pointers/comparingPointers.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  
-  cout << "";
+//in pointers you're essentially comparing addresses
 
+int main() {
+  int numbers[] = {10, 20, 30};
+  int* ptr = &numbers[size(numbers) - 1];
+
+  //comparing two pointers
+  while(ptr >= numbers){
+    cout << *ptr << " ";
+    ptr--;
+  }
+ 
   return 0;
 }

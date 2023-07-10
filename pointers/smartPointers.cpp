91615ff3a3940f90361dcdde8b6522c33da633c4
gnunez-dev <gnunez.dev@gmail.com>
2023-07-09 21:55:25 -0500

    added smartPointers

--- pointers/smartPointers.cpp
+++ pointers/smartPointers.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main() {
   
-  cout << "";
+  int* x = new int;
+  delete x;
+  delete x; //this will cause the program to crash
 
   return 0;
 }
+
+/* 
+  Smart pointers:
+  - Unique pointers
+  - Shared pointers
+ */
